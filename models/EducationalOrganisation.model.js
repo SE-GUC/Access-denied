4a398da084c187e8e167153a75db451555580763
CarolGuirguis <katty_guirguis@hotmail.com>
2019-02-28 00:24:20 +0200

    CR for Educational Organization

---
+++ models/EducationalOrganisation.model.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+const uri="mongodb+srv://MubbyGN:nwoFDbE9QE6z0iEE@se-qt9vz.mongodb.net/test?retryWrites=true"
+
+mongoose.connect(uri,{useNewUrlParser:true})
+
+const EducationalOrganisationSchema=new mongoose.Schema({
+    name:{
+       type:String,
+       required:true,
+    },
+    address:String,
+    email:{
+       type:String,
+       required:true,
+       unique:true
+    },
+    contactInformation:String,
+    vision:String,
+    mission:String,
+    partners:[String],
+    information:String,
+    course:[String],
+    certificate:[String],
+    trainer:[String],
+    trainingProgram:[String]
+     } )
+ module.exports = mongoose.model("EducationalOrganisation", EducationalOrganisationSchema)
