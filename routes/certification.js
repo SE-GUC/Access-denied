593b5aeeb9d7811e9f885c0b0a98f9ffeb11678e
Omar-Handouk <o.i.handouk@gmail.com>
2019-03-01 07:33:22 -0800

    [*] Modified index [*] Modified certification [-] Deleted taskRoute [+] Added task

--- routes/certification.js
+++ routes/certification.js
@@ -1,20 +1,19 @@
-
 "use strict";
 const certificationModel = require('../models/certification.model');
 const express = require('express');
 const router = express.Router();
 
-
-
-
 router.post("/", (req, res) => {
-    if(!req.body){
+
+    if (!req.body) {
         return res.status(400).send("Body is missing")
     }
-    let model = new certificatioModel(req.body)
+
+    let model = new certificationModel(req.body)
+
     model.save()
         .then((doc) => {
-            if(!doc || doc.length ===0){
+            if (!doc || doc.length === 0) {
                 return res.status(500).send(doc)
             }
 
@@ -26,14 +25,16 @@ router.post("/", (req, res) => {
 })
 
 router.put('/', (req, res) => {
-    if(!req.query.id_of_certification){
+
+    if (!req.query.id_of_certification) {
         return res.status(400).send('id of certification is mising.')
     }
+
     certificationModel.findOneAndUpdate({
-        id_of_certification: req.query.id_of_certification
-    }, req.body, {
-        new: true
-    })
+            id_of_certification: req.query.id_of_certification
+        }, req.body, {
+            new: true
+        })
         .then((doc) => {
             res.json(doc)
         })
@@ -43,22 +44,33 @@ router.put('/', (req, res) => {
 })
 
 router.delete('/', (req, res) => {
-    if(!req.query.id_of_certification){
+    if (!req.query.id_of_certification) {
         return res.status(400).send('id is mising.')
     }
     certificationModel.findOneAndDelete({
+
+            id_of_certification: req.query.id_of_certification
+        })
+        .then((doc) => {
+            res.json(doc)
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+})
+
 
 
 
 
 router.get("/", (req, res) => {
-    if(!req.query.id_of_certification){
+    if (!req.query.id_of_certification) {
         return res.status(400).send("ID of certification is mising.")
     }
-    certificatioModel.find({
+    certificationModel.find({
 
-        id_of_certification: req.query.id_of_certification
-    })
+            id_of_certification: req.query.id_of_certification
+        })
         .then((doc) => {
             res.json(doc)
         })
