a7c9edf9d5fef840dccb1334627bd6ce5b3ef198
ahmed-elzamarany <ahmed_elzamarany@hotmail.com>
2019-03-16 00:31:30 +0200

    new schema

--- models/partner.model.js
+++ models/partner.model.js
@@ -3,39 +3,74 @@ const uri = "mongodb+srv://MubbyGN:nwoFDbE9QE6z0iEE@se-qt9vz.mongodb.net/test?re
 mongoose.connect(uri, {useNewUrlParser:true})
 
 const PartnerSchema = new mongoose.Schema({
-    //basic business information 
+    //*basic business information :
+        // 1- name of company,
     name: {
         type: String,
         required: true,
         unique: true
     },
+         //2- contact info, 
     email: {
+        type: String,
+        required: false,
+        unique: true
+    },
+    Telephone_number: {
+        type: number,
+        required: false,
+        unique: false
+    },
+                //other contact links (social media)
+    other: [ {
+        name:String,
+    }],
+    //*location
+    location: {
         type: String,
         required: true,
         unique: true
     },
+    //*number of employees
+    number_of_employees:{
+        type:Number,
+        required: false,
+        unique: false
+    },
+
+
+    //*their past projects ?
+   
+   //*their filed of work
     field_of_work: {
         type: String,
         required: true,
-        unique: true
+        unique: false
     },
-    //their partners
+    //*their partners?
     other_partner:{
+<<<<<<< HEAD
         type: String,
         unique: true
+=======
+        type:Number,
+        unique: false
+>>>>>>> new schema
     },
-    //board members
+    //*their board members
     member :[ {
         name:String,
         age:Number,
-        email:String
+        email:String,
+        past_work:String
     }],
-    //events
+    //*events organized by the organization?
     events :[ {
         name:String,
         location:String,
         email:String
     }]
 })
+//adding a form to suggest any feedback.
 
 module.exports = mongoose.model("Partners", PartnerSchema)
