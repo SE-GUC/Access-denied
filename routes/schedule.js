ac86ad1afdd56db9597994537209dede1df34ed9
mahmo0odSalah <medoysf@gmail.com>
2019-03-20 13:57:12 +0200

    fix Create

--- routes/schedule.js
+++ routes/schedule.js
@@ -9,15 +9,15 @@ router.post('/',(req,res)=>{
     //Add validation here
     let model =new ScheduleModel(req.body);
     model.save()
-    .then((doc)=>{
-        if(!doc || doc.length ===0){
-            return res.status(500).send(doc)
-        }
-        res.send(201).send(doc)
-    })
-    .catch((err)=>{
-        res.status(500).json(err)
-    })
+        .then((doc)=>{
+            if(!doc || doc.length ===0){
+                return res.status(500).send(doc)
+            }
+            res.status(201).send(doc)
+        })
+        .catch((err)=>{
+            res.status(500).json(err)
+        })
 })
 
 module.exports = router;
