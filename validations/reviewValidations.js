0de1ea9eccde83a543aef19add6c65b7fb582e6d
mubbygn <mubbyashraf@gmail.com>
2019-03-22 18:33:05 +0200

    Adding review validations

--- validations/reviewValidations.js
+++ validations/reviewValidations.js
@@ -1,25 +1,39 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 module.exports = {
-    createValidation: request => {
-        const createSchema = {
-            reviewer: Joi.required(),
-            reviewee: Joi.required(),
-            review: Joi.string().min(3).max(500).required(),
-            rating: Joi.number().min(1).max(5).required()
-        }
+  createValidation: request => {
+    const createSchema = {
+      reviewer: Joi.required(),
+      reviewee: Joi.required(),
+      review: Joi.string()
+        .min(3)
+        .max(500)
+        .required(),
+      rating: Joi.number()
+        .min(1)
+        .max(5)
+        .required(),
+      task: Joi,
+      onModel: Joi.string()
+    };
 
-        return Joi.validate(request, createSchema)
-    },
+    return Joi.validate(request, createSchema);
+  },
 
-    updateValidation: request => {
-        const updateSchema = {
-            reviewer: Joi,
-            reviewee: Joi,
-            review: Joi.string().min(3).max(500),
-            rating: Joi.number().min(1).max(5)
-        }
+  updateValidation: request => {
+    const updateSchema = {
+      reviewer: Joi,
+      reviewee: Joi,
+      review: Joi.string()
+        .min(3)
+        .max(500),
+      rating: Joi.number()
+        .min(1)
+        .max(5),
+      task: Joi,
+      onModel: Joi.string()
+    };
 
-        return Joi.validate(request, updateSchema)
-    }, 
-}
+    return Joi.validate(request, updateSchema);
+  }
+};
