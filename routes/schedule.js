a995ee44ec21bd2a6f4b42c825d82a4b8d2cbc3f
mubbygn <mubbyashraf@gmail.com>
2019-03-23 22:53:38 +0200

    change require

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,4 +1,4 @@
-const ScheduleModel = require("../models/schedule.model");
+const ScheduleModel = require("../models/Schedule.model");
 const express = require("express");
 const router = express.Router();
 const validator = require("../validations/scheduleValidations");
