24c0c7906bc7d5152fd36ef27a1540a235497764
mahmo0odSalah <medoysf@gmail.com>
2019-03-31 00:19:32 +0200

    Prettifying

--- routes/member.js
+++ routes/member.js
@@ -97,7 +97,8 @@ router.get('/', (req, res) => {
   memberModel
     .findOne({
       email: req.query.email
-    }).select("-password")
+    })
+    .select('-password')
     .then(doc => {
       res.json(doc)
     })
@@ -109,7 +110,8 @@ router.get('/all', (_request, response) => {
   let key = {}
 
   memberModel
-    .find(key).select("-password")
+    .find(key)
+    .select('-password')
     .then(document => {
       if (!document || document.length == 0) {
         return response.status(500).json(document)
@@ -232,25 +234,30 @@ router.get('/applyonTask', (request, response) => {
     })
 })
 
-router.post("/adddate", (req,res)=>{
-  if(!req.query.email) return res.status(400).send("Email is missing")
-  if(!req.body.date) return res.status(400).send("Date is missing")
-  memberModel.findOneAndUpdate({
-    "email":req.query.email
-  }, {
-    $push:{"calendar":req.body.date}
-  },{
-    new:true,
-    safe:true,
-    upsert:true
-  }
-  ).then((doc)=>{
-    if(!doc || doc.length === 0) return res.status(500).send("Error")
-    res.json(doc)
-  })
-  .catch(err => {
-    return res.status(500).json(err)
-  })
+router.post('/adddate', (req, res) => {
+  if (!req.query.email) return res.status(400).send('Email is missing')
+  if (!req.body.date) return res.status(400).send('Date is missing')
+  memberModel
+    .findOneAndUpdate(
+      {
+        email: req.query.email
+      },
+      {
+        $push: { calendar: req.body.date }
+      },
+      {
+        new: true,
+        safe: true,
+        upsert: true
+      }
+    )
+    .then(doc => {
+      if (!doc || doc.length === 0) return res.status(500).send('Error')
+      res.json(doc)
+    })
+    .catch(err => {
+      return res.status(500).json(err)
+    })
 })
 
 module.exports = router
