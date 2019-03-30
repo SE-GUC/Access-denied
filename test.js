1f9e6bc0907d6431e05c94b0e826fa4ceda142b1
mahmo0odSalah <medoysf@gmail.com>
2019-03-30 19:42:30 +0200

    added schedule tests

--- test.js
+++ test.js
@@ -4,3 +4,4 @@
 const coworkingTests = require('./tests/coworkingTest')
 const consultacyTests = require('./tests/consultancyTest')
 const reviewTests = require('./tests/reviewTest')
+const scheduleTests = require('./tests/scheduleTest')
