0483ed89c1f17d578d38d2df2e3898fe661a42e5
mubbygn <mubbyashraf@gmail.com>
2019-03-31 20:22:53 +0200

    Making tests independent

--- tests/partnerFeedbackTest.js
+++ tests/partnerFeedbackTest.js
@@ -5,18 +5,33 @@ const _ = require('lodash')
 const testreviewee = '5c9519e7cef4f345903ba59d'
 var baseURL = process.env.BASEURL || 'http://localhost:3000'
 let reviewreply = ''
-
+let task = ''
 const postFeedbackTest = test('post feedback', async () => {
+  const document = {
+    name: 'Tester.Test123',
+    owner: '5c9519e7cef4f345903ba59d',
+    assignee: '5c951a6acef4f345903ba59e',
+    description: 'This is Tester.Test123 Description',
+    extraNotes: 'This is Tester.Test123 Extra Notes',
+    effortLevel: 999999999,
+    monetaryComp: 999999999,
+    isComplete: true
+  }
+  task = await axios.post(`${baseURL}/api/task`, document)
   const body = {
     reviewer: '5c951a6acef4f345903ba59e',
     reviewee: '5c9519e7cef4f345903ba59d',
     review: 'test',
     rating: 3,
-    task: '5ca0011e0eb0f426b03291ac',
+    task: task.data._id,
     reviewerModel: 'Members',
     revieweeModel: 'Partners'
   }
   reviewreply = (await axios.post(`${baseURL}/api/review`, body)).data
+  let docID = task.data._id
+  let deleteresponse = await axios.delete(
+    `${baseURL}/api/task` + `?id=${docID}`
+  )
   expect(_.pick(reviewreply, Object.keys(body))).toEqual(body)
 })
 
@@ -26,7 +41,7 @@ const viewFeedbackTest = test('view feedback', async () => {
     reviewee: '5c9519e7cef4f345903ba59d',
     review: 'test',
     rating: 3,
-    task: '5ca0011e0eb0f426b03291ac',
+    task: task.data._id,
     reviewerModel: 'Members',
     revieweeModel: 'Partners'
   }
@@ -47,7 +62,7 @@ const deleteFeedbackTest = test('delete feedback', async () => {
     reviewee: '5c9519e7cef4f345903ba59d',
     review: 'test',
     rating: 3,
-    task: '5ca0011e0eb0f426b03291ac',
+    task: task.data._id,
     reviewerModel: 'Members',
     revieweeModel: 'Partners'
   }
