706b295ed145c2a2c9ee784911e3a2d35c37b27d
mahmo0odSalah <medoysf@gmail.com>
2019-03-31 17:36:49 +0200

    added password hashing

--- models/member.Model.js
+++ models/member.Model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const MemberSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -26,4 +26,30 @@ const MemberSchema = new mongoose.Schema({
   ],
   calendar: [Date]
 })
+
+MemberSchema.pre('save', function(next) {
+  let user = this
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next()
+
+  bcrypt
+    .hash(user.password, 2)
+    .then(hash => {
+      user.password = hash
+    })
+    .catch(err => {
+      return next(err)
+    })
+  next()
+})
+
+MemberSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt
+    .compare(candidatePassword, this.password)
+    .then(isMatch => {
+      cb(null, isMatch)
+    })
+    .catch(err => cb(err))
+}
 module.exports = mongoose.model('Members', MemberSchema)
