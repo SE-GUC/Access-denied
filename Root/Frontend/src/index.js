e13e6f77747ed682fba635292e078c13603550c5
ZainabSultan <zainabsultan1111@gmail.com>
2019-04-06 23:51:44 +0300

    SignUp fixing

--- Root/Frontend/src/index.js
+++ Root/Frontend/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import SignUp from "./Screens/SignUp";
 // ReactDOM.render(<SearchPage />, document.getElementById('root'));
 //ReactDOM.render(<SignUp />, document.getElementById("root"));
-
+//ReactDOM.render(<LogIn />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
