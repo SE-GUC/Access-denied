e0734648338463b83a7473ead5ff4193e146edb8
ahmed-elzamarany <ahmed_elzamarany@hotmail.com>
2019-04-07 18:23:50 +0200

    putting backend

---
+++ Root/Frontend/src/Components/results.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import C from './card';
+
+class results extends Component {
+
+  render() {
+    return (
+        this.props.results.map(p=>{
+            return(
+                <div> <C content={p.description} title={p.name} image={null}  />
+                </div>
+            )}) 
+     
+       
+      
+    
+ )
+}} 
+
+export default results ;
--- Root/Frontend/src/Screens/SearchPage.js
+++ Root/Frontend/src/Screens/SearchPage.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import '../Screens/SearchPage.css';
 import S from '../Components/searchbar';
 import F from '../Components/filterPanel';
+import C from '../Components/results';
 
 class SearchPage extends Component {
+
+  state = {
+    results: []
+      }
+
+      async GetTasks() {
+        axios.get(`http://localhost:3001/api/task/all`)
+          .then(res => {
+            const task = res.data;
+            this.setState( {tasks:task} );
+          })}
+          
   render() {
+    await this.GetTasks();
+
     return (
      <div>
        <div><S/></div>
         <div><F/></div>
+        <C results={this.state.results} />
       </div>
       
     )
