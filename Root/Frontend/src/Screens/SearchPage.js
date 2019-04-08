10215da7ca862940edc56e38656f7aace04dbf80
mahmo0odSalah <medoysf@gmail.com>
2019-04-08 21:46:10 +0200

    bug fix

--- Root/Frontend/src/Screens/SearchPage.js
+++ Root/Frontend/src/Screens/SearchPage.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 // import '../Screens/SearchPage.css';
-import S from '../Components/searchbar';
-import F from '../Components/filterPanel';
-import C from '../Components/results';
+import S from "../Components/searchbar";
+import F from "../Components/filterPanel";
+import C from "../Components/results";
 
 class SearchPage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.SetResults = this.SetResults.bind(this)
+    this.SetResults = this.SetResults.bind(this);
   }
   state = {
-    results: []      }
+    results: []
+  };
 
-        componentDidMount() {
-       axios.get(`http://localhost:3001/api/task/all`)
-       .then(res=>{
-        this.state={
-          results:res.data
-        }
-       })
-       }
+  componentDidMount() {
+    axios.get(`http://localhost:3001/api/task/all`).then(res => {
+      this.state = {
+        results: res.data
+      };
+    });
+  }
 
+  SetResults(res) {
+    this.state = {
+      results: res
+    };
+    console.log(this.state.results);
+  }
 
-      SetResults (res){
-        this.state={
-          results:res
-        }
-        console.log(this.state.results)
-      }
-
-
-          
   render() {
-  //  await this.GetTasks();
+    //  await this.GetTasks();
 
     return (
-     <div>
-        <div><F change={this.SetResults} /></div>
+      <div>
+        <div>
+          <F change={this.SetResults} />
+        </div>
         <C results={this.state.results} />
       </div>
     );
