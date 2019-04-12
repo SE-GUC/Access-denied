11d3981f71c799502f1966119a955ae5e4dcadad
mubbygn <mubbyashraf@gmail.com>
2019-04-13 00:17:03 +0200

    Tests Collections drop

--- test.js
+++ test.js
@@ -3,6 +3,22 @@
  * @jest-environment node
 //  */
 
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true })
+beforeAll(() => {
+  mongoose.connection.dropDatabase(error => {
+    if (error) console.log(error)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.dropDatabase(error => {
+    if (error) console.log(error)
+  })
+  mongoose.disconnect()
+})
+
 const coworkingTests = require('./tests/coworkingTest')
 const consultacyTests = require('./tests/consultancyTest')
 const reviewTests = require('./tests/reviewTest')
