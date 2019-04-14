b37e893a6223f6213c3eccfb486a871e280e3bc8
mubbygn <mubbyashraf@gmail.com>
2019-04-14 20:22:59 +0200

    Prettifying

--- routes/requests.js
+++ routes/requests.js
@@ -12,11 +12,10 @@ const fetch = require('node-fetch')
 
 //TODO:
 //1-all post and pull routes should check if its admin or not
-//if it's admin it will do its job 
-//if not it will make post request to this API 
+//if it's admin it will do its job
+//if not it will make post request to this API
 //2-add admin user story that take a request from the requests model then send it
 //3-add validations DONE
-
 
 /**
  * @description Post Document in Database
@@ -29,17 +28,16 @@ router.post('/', (request, response) => {
     return response.status(400).send('400: token_id is missing')
   }
 
+  let doc = request.body
+  doc['requester'] = request.query.token_id //TODO :ref partner, members , users of the system
+  const isValidated = validator.createValidation(doc)
 
-let doc =request.body
-doc['requester'] = request.query.token_id; //TODO :ref partner, members , users of the system
-const isValidated = validator.createValidation(doc)
-
-if (isValidated.error) {
-  return response.status(400).send({
-    error: isValidated.error.details[0].message
-  })
-}
-Request.create(doc)
+  if (isValidated.error) {
+    return response.status(400).send({
+      error: isValidated.error.details[0].message
+    })
+  }
+  Request.create(doc)
     .then(document => {
       if (!document || document.length === 0) {
         return response.status(500).json(document)
@@ -65,7 +63,6 @@ Request.create(doc)
  */
 
 router.get('/', (request, response) => {
-
   let documentID = request.query.id
 
   if (!documentID) {
@@ -107,108 +104,103 @@ router.get('/all', (request, response) => {
 })
 
 router.put('/', (request, response) => {
-    let documentID = request.query.id
-  
-    if (!documentID) {
-      return response
-        .status(400)
-        .status('400: Bad Request, no document ID is supplied')
-    }
-  
-    const isValidated = validator.updateValidation(request.body)
-  
-    if (isValidated.error) {
-      return response.status(400).send({
-        error: isValidated.error.details[0].message
-      })
-    }
-  
-    let key = {
-      _id: documentID
-    }
-  
-    let updatedDocument = request.body
-  
-    Request.findOneAndUpdate(key, updatedDocument, {
-      new: true
+  let documentID = request.query.id
+
+  if (!documentID) {
+    return response
+      .status(400)
+      .status('400: Bad Request, no document ID is supplied')
+  }
+
+  const isValidated = validator.updateValidation(request.body)
+
+  if (isValidated.error) {
+    return response.status(400).send({
+      error: isValidated.error.details[0].message
     })
-      .then(document => {
-        if (!document || document.length == 0) {
-          return response.status(500).json(document)
-        }
-  
-        response.json(document)
-      })
-      .catch(error => {
-        response.status(500).json(error)
-      })
-  })
-  
+  }
 
-  router.delete('/', (request, response) => {
-    let documentID = request.query.id
-  
-    if (!documentID) {
-      return response
-        .status(400)
-        .status('400: Bad Request, no document ID is supplied')
-    }
-  
-    let key = {
-      _id: documentID
-    }
-  
-    Request.findOneAndDelete(key)
-      .then(document => {
-        if (!document || document.length == 0) {
-          return response.status(500).json(document)
-        }
-  
-        response.json(document)
-      })
-      .catch(error => {
-        response.status(500).json(error)
-      })
-  })
+  let key = {
+    _id: documentID
+  }
 
-  
-  //Admin story for accepting requests and resend it 
+  let updatedDocument = request.body
+
+  Request.findOneAndUpdate(key, updatedDocument, {
+    new: true
+  })
+    .then(document => {
+      if (!document || document.length == 0) {
+        return response.status(500).json(document)
+      }
+
+      response.json(document)
+    })
+    .catch(error => {
+      response.status(500).json(error)
+    })
+})
+
+router.delete('/', (request, response) => {
+  let documentID = request.query.id
+
+  if (!documentID) {
+    return response
+      .status(400)
+      .status('400: Bad Request, no document ID is supplied')
+  }
+
+  let key = {
+    _id: documentID
+  }
+
+  Request.findOneAndDelete(key)
+    .then(document => {
+      if (!document || document.length == 0) {
+        return response.status(500).json(document)
+      }
+
+      response.json(document)
+    })
+    .catch(error => {
+      response.status(500).json(error)
+    })
+})
+
+//Admin story for accepting requests and resend it
 router.get('/Approve', (request, response) => {
+  let documentID = request.query.id
 
-    let documentID = request.query.id
-  
-    if (!documentID) {
-      return response
-        .status(400)
-        .status('400: Bad Request, no document ID is supplied')
-    }
-  
-    let key = {
-      _id: documentID
-    }
-  
-    Request.findOne(key)
-      .then(document => {
-        if (!document || document.length == 0) {
-          return response.status(500).json(document)
-        }
-        console.log(baseURL+'/'+document.route)
-          fetch(baseURL+'/'+document.route, {
-            method: document.type,
-            body: JSON.stringify(document.body),
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin'
-          }) 
-          .then(res => {
-              console.log(res.status)
-            response.send(res.status)
-          })
+  if (!documentID) {
+    return response
+      .status(400)
+      .status('400: Bad Request, no document ID is supplied')
+  }
 
+  let key = {
+    _id: documentID
+  }
+
+  Request.findOne(key)
+    .then(document => {
+      if (!document || document.length == 0) {
+        return response.status(500).json(document)
+      }
+      console.log(baseURL + '/' + document.route)
+      fetch(baseURL + '/' + document.route, {
+        method: document.type,
+        body: JSON.stringify(document.body),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+      }).then(res => {
+        console.log(res.status)
+        response.send(res.status)
       })
-      .catch(error => {
-        response.status(500).json(error)
-      })
-  })
-module.exports =router
+    })
+    .catch(error => {
+      response.status(500).json(error)
+    })
+})
+module.exports = router
--- validations/requestValidations.js
+++ validations/requestValidations.js
@@ -3,13 +3,12 @@ const Joi = require('joi')
 module.exports = {
   createValidation: request => {
     const createSchema = {
-        requester: Joi.string(),
-        route: Joi.string(),
-        type: Joi.string(),
-        description: Joi.string(),
-        Date: Joi.date(),
-        body: Joi.object()
-       
+      requester: Joi.string(),
+      route: Joi.string(),
+      type: Joi.string(),
+      description: Joi.string(),
+      Date: Joi.date(),
+      body: Joi.object()
     }
 
     return Joi.validate(request, createSchema)
@@ -17,14 +16,12 @@ module.exports = {
 
   updateValidation: request => {
     const updateSchema = {
-        requester: Joi.string(),
-        route: Joi.string(),
-        type: Joi.string(),
-        description: Joi.string(),
-        Date: Joi.date(),
-        body: Joi.object()
-
-       
+      requester: Joi.string(),
+      route: Joi.string(),
+      type: Joi.string(),
+      description: Joi.string(),
+      Date: Joi.date(),
+      body: Joi.object()
     }
 
     return Joi.validate(request, updateSchema)
