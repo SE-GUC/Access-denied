1e73970cdf0d39a6141ae1c13d7ad5a13bd363da
mubbygn <mubbyashraf@gmail.com>
2019-04-15 02:22:34 +0200

    Chat Socket implementation

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { Launcher } from "react-chat-window";
-import Dialog from "./Dialog";
-import io from "socket.io-client";
-import { AppConsumer } from "../Containers/AppProvider";
+import React, { Component } from 'react'
+import { Launcher } from 'react-chat-window'
+import Dialog from './Dialog'
+import io from 'socket.io-client'
+import { AppConsumer } from '../Containers/AppProvider'
 
 class Chat extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       messageList: [],
       them: null,
@@ -14,61 +14,44 @@ class Chat extends Component {
       token: null,
       id: null,
       changed: false
-    };
+    }
   }
   getMessages() {
-    if (!this.state.them) return;
-    let id = this.state.id;
+    if (!this.state.them) return
+    let id = this.state.id
     fetch(`/api/message/convo?id1=${id}&id2=${this.state.them}`)
       .then(res => res.json())
       .then(messages => {
-        messages.sort((a, b) => new Date(a.date) - new Date(b.date));
-        let currentState = this.state;
+        messages.sort((a, b) => new Date(a.date) - new Date(b.date))
+        let currentState = this.state
         currentState.messageList = messages.map(message => {
-          let author = message.from === id ? "me" : "them";
+          let author = message.from === id ? 'me' : 'them'
           return {
             author: author,
-            type: "text",
+            type: 'text',
             data: { text: message.message }
-          };
-        });
-        this.setState(currentState);
-      });
+          }
+        })
+        this.setState(currentState)
+      })
   }
   componentDidMount() {
-    let id = this.state.id;
+    let id = this.state.id
     if (id) {
-      this.getMessages();
+      this.getMessages()
       fetch(`/api/message?id=${id}`)
         .then(res => res.json())
         .then(chats => {
-          this.setState({ chats: chats });
-        });
+          this.setState({ chats: chats })
+        })
     }
-    var socket = io();
-    socket.on(id, obj => {
-      if (obj.from === this.state.them) {
-        this.setState({
-          messageList: [
-            ...this.state.messageList,
-            {
-              author: obj.from === id ? "me" : "them",
-              type: "text",
-              data: { text: obj.message }
-            }
-          ]
-        });
-      } else {
-        this.props.handleNotification(true);
-      }
-    });
   }
 
   _onMessageWasSent(message) {
-    fetch("/api/message", {
-      method: "POST",
+    fetch('/api/message', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         from: this.state.token,
@@ -76,10 +59,10 @@ class Chat extends Component {
         date: new Date(),
         message: message.data.text
       })
-    }).then(data => console.log(data));
+    }).then(data => console.log(data))
     this.setState({
       messageList: [...this.state.messageList, message]
-    });
+    })
   }
 
   _sendMessage(text) {
@@ -88,34 +71,51 @@ class Chat extends Component {
         messageList: [
           ...this.state.messageList,
           {
-            author: "them",
-            type: "text",
+            author: 'them',
+            type: 'text',
             data: { text }
           }
         ]
-      });
+      })
     }
   }
   async handleChat(chat) {
-    await this.setState({ them: chat });
-    this.getMessages();
+    await this.setState({ them: chat })
+    this.getMessages()
   }
   render() {
     return (
       <div>
         <AppConsumer>
           {context => {
-            if (this.state.changed || !context.token) return;
+            if (this.state.changed || !context.token) return
             this.setState({
               token: context.token,
               id: context.id,
               changed: true
-            });
+            })
             fetch(`/api/message?id=${context.id}`)
               .then(res => res.json())
               .then(chats => {
-                this.setState({ chats: chats });
-              });
+                this.setState({ chats: chats })
+              })
+            var socket = io()
+            socket.on(context.id, obj => {
+              if (obj.from === this.state.them) {
+                this.setState({
+                  messageList: [
+                    ...this.state.messageList,
+                    {
+                      author: obj.from === context.id ? 'me' : 'them',
+                      type: 'text',
+                      data: { text: obj.message }
+                    }
+                  ]
+                })
+              } else {
+                this.props.handleNotification(true)
+              }
+            })
           }}
         </AppConsumer>
         {!this.state.them ? (
@@ -125,7 +125,7 @@ class Chat extends Component {
             agentProfile={{
               teamName: this.state.them,
               imageUrl:
-                "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png"
+                'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
             }}
             onMessageWasSent={this._onMessageWasSent.bind(this)}
             messageList={this.state.messageList}
@@ -137,8 +137,8 @@ class Chat extends Component {
           handleChat={this.handleChat.bind(this)}
         />
       </div>
-    );
+    )
   }
 }
 
-export default Chat;
+export default Chat
